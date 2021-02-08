d7c56584c0be19494ccb1977672402f32f5db7c0
hasantezcan <hasantezcan77@gmail.com>
2021-02-09 02:52:47 +0300

    Add aboutUs page

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import UserWelcome from "./pages/user/pages/UserWelcome/index";
 import LogIn from "./pages/auth/LogIn";
 import Register from "./pages/auth/Register";
 import Navbar from "./shared/components/Navbar/index";
+import AboutUs from './pages/AboutUs/index';
 
 const App = () => (
 	<div className="app">
 		<Router>
-		<Navbar />
+			<Navbar />
 			<div>
 				<div className="content">
 					<Switch>
 						<Route path="/" exact component={UserWelcome} />
 						<Route path="/login" component={LogIn} />
 						<Route path="/register" component={Register} />
+						<Route path="/aboutUs" component={AboutUs} />
 					</Switch>
 				</div>
 			</div>
