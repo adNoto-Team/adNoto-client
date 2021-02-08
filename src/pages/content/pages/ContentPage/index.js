a8ba598523f08ef693c4acfa323098e44f340da8
hasantezcan <hasantezcan77@gmail.com>
2021-02-09 02:02:09 +0300

    Create content's boilerpalte files

---
+++ src/pages/content/pages/ContentPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./style.module.css"
+
+function ContentPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ContentPage
---
+++ src/pages/content/pages/EpisodePage      /index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./style.module.css";
+
+function EpisodePage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default EpisodePage
