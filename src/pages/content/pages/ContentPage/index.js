9c10ec2ca640bf536dfe4a4aa2e2e6a499ba8025
hasantezcan <hasantezcan77@gmail.com>
2021-02-11 14:46:11 +0300

    Create contentPage's boilerplate

--- src/pages/content/pages/ContentPage/index.js
+++ src/pages/content/pages/ContentPage/index.js
@@ -1,7 +1,19 @@
+import { Layout, Col } from "antd";
+
 import style from "./style.module.css";
 
+const { Content } = Layout;
+
 const ContentPage = () => {
-	return <div></div>;
+	return <>
+			<Col lg={{ span: 12, offset: 6 }}>
+				<Layout span={12} offset={6}>
+					<Content>
+					<h1>Content Page</h1>
+					</Content>
+				</Layout>
+			</Col>
+		</>;
 };
 
 export default ContentPage;
