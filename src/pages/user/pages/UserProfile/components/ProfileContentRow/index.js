6a6556f2241acb8df4e65c706e7006d166dd57d5
hasantezcan <hasantezcan77@gmail.com>
2021-02-12 10:30:17 +0300

    Complete user profile page

---
+++ src/pages/user/pages/UserProfile/components/ProfileContentRow/index.js
@@ -0,0 +1,27 @@
+import { Row } from "antd";
+
+import CustomDivider from '../../../../../../shared/components/CustomDivider/index';
+import CardItem from "./CardItem";
+import style from "./style.module.css";
+
+const ProfileContentRow = ({ title, contents }) => {
+	return (
+		<>
+			<Row style={{ paddingTop: 30 }} justify={"center"}>
+				<CustomDivider title={title} />
+
+				<Row gutter={[50, 50]} className={style.contentRow}>
+					{contents.map((content) => (
+						<CardItem
+							title={content.title}
+							imdb={content.imdb}
+							coverImg={content.coverImg}
+						/>
+					))}
+				</Row>
+			</Row>
+		</>
+	);
+};
+
+export default ProfileContentRow;
